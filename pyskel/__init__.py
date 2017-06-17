e6a8358210781555f1855371b9a5be9359a2c92e
Jaime Ashander <jashander@ucdavis.edu>
2017-06-16 17:35:05 -0700

    Example of click app using multiproc

--- pyskel/__init__.py
+++ pyskel/__init__.py
@@ -1,3 +1,29 @@
 # pyskel
+import time
+from random import random
 
 has_legs = False
+
+
+class Simulation(object):
+    def __init__(self, pars):
+        self.pars = pars
+        self.pd = None
+
+
+def mod_run(sim_p_tuple):
+    sim, p = sim_p_tuple
+    _, p2 = sim.pars
+    sim.pars = p, p2
+    return run(sim)
+
+
+def run(sim):
+    pd = []
+    p1, p2 = sim.pars
+    for ctr in range(3):
+        pd.append([p1, p2])
+        time.sleep(random() * 5)
+
+    print(pd)
+    return "done"
--- pyskel/scripts/cli.py
+++ pyskel/scripts/cli.py
@@ -1,13 +1,35 @@
 # Skeleton of a CLI
 
 import click
-
 import pyskel
+import multiprocessing
 
 
-@click.command('pyskel')
+@click.group(name='pyskel', chain=True)
 @click.argument('count', type=int, metavar='N')
-def cli(count):
+@click.pass_context
+def cli(ctx, count):
     """Echo a value `N` number of times"""
-    for i in range(count):
-        click.echo(pyskel.has_legs)
+    p1 = count
+    p2 = tuple(range(5))
+    ctx.obj = pyskel.Simulation((p1, p2))
+
+
+@cli.command()
+@click.option('--p1-val', type=float)
+@click.pass_obj
+def run(sim, p1_val):
+    pyskel.mod_run((sim, p1_val))
+
+
+@cli.command()
+@click.option('--p1-range', type=(int, int, int),
+              help="tuple passed is called with range(first, second, step)")
+@click.pass_obj
+def run_several_par1(sim, p1_range):
+    first, last, step = p1_range
+    p1_vals = list(range(first, last, step))
+
+    pool = multiprocessing.Pool(3)
+    res = pool.map(pyskel.mod_run, ((sim, p) for p in p1_vals))
+    print(res)
